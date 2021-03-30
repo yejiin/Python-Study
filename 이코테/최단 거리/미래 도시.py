60c39cb64f9e3f6d81b0adb70d3db44d5310616c
yjsong67 <yj_song67@naver.com>
2021-03-30 09:03:08 +0900

    미래 도시

---
+++ 이코테/최단 거리/미래 도시.py
@@ -0,0 +1,29 @@
+INF = int(1e9)
+
+N, M = map(int, input().split())
+
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for i in range(1, N + 1):
+    for a in range(1, N + 1):
+        for b in range(1, N + 1):
+            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+X, K = map(int, input().split())
+
+result = graph[1][K] + graph[K][X]
+
+if result < INF:
+    print(result)
+else:
+    print(-1)
