b9aef38a1aae99d7a3b41af6cc101d5acb003b3c
yejiin <yj_song67@naver.com>
2021-05-03 21:22:47 +0900

    행렬의 덧셈

---
+++ Programmers/Lv1/행렬의 덧셈.py
@@ -0,0 +1,7 @@
+def solution(arr1, arr2):
+    answer = [[] * i for i in range(len(arr1))]
+
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            answer[i].append(arr1[i][j] + arr2[i][j])
+    return answer
