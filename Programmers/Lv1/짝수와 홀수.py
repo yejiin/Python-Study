def19792de7291d8ce1bcfea15900bf3523b86cf
yejiin <yj_song67@naver.com>
2021-05-22 18:38:11 +0900

    짝수와 홀수

---
+++ Programmers/Lv1/짝수와 홀수.py
@@ -0,0 +1,10 @@
+def solution(num):
+
+    return "Odd" if num % 2 == 1 else "Even"
+
+    # if num % 2 == 0:
+    #     answer = "Odd"
+    # else:
+    #     answer = "Even"
+    # return answer
+
