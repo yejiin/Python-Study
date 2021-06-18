d49a400f4300df0f76379fb9f66fa6c52ac2a809
yejiin <yj_song67@naver.com>
2021-06-18 09:52:49 +0900

    여행 계획

---
+++ 이코테/그래프 이론/여행 계획.py
@@ -0,0 +1,39 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+
+parent = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    for j in range(n):
+        if data[j] == 1:
+            union_parent(parent, i + 1, j + 1)
+
+plan = list(map(int, input().split()))
+
+result = True
+# 여행 계획에 속하는 모든 노드의 루트가 동일한지 확인
+for i in range(m - 1):
+    if plan[i] != plan[i + 1]:
+        result = False
+
+if result:
+    print("YES")
+else:
+    print("NO")
